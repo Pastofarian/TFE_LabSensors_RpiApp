95e94c72d9382f76e445ea088a5ab3bc15dd5f44
Pastofarian <asegliochristophe@gmail.com>
2024-11-01 20:43:44 +0100

    refactor function lab_datas_db

--- rpi_app.py
+++ rpi_app.py
@@ -30,13 +30,17 @@ def lab_datas():
 # route to get datas from the db
 @app.route("/lab_datas_db", methods=['GET'])
 def lab_datas_db():
-    from_date = request.args.get('from', time.strftime("%Y-%m-%d %H:%M"))  # Get the from date value from the URL
-    to_date = request.args.get('to', time.strftime("%Y-%m-%d %H:%M"))  # Get the to date value from the URL
+    temperatures, humidities, from_date, to_date = get_datas()
+    return render_template("lab_datas_db.html", temp=temperatures, hum=humidities)
+
+def get_datas():
+    from_date = request.args.get('from', time.strftime("%Y-%m-%d %H:%M:%S")) # Get the from date value from the URL
+    to_date = request.args.get('to', time.strftime("%Y-%m-%d %H:%M:%S")) # Get the to date value from the URL
 
     if not check_date(from_date):  # Validate date before sending it to the DB
-        from_date = time.strftime("%Y-%m-%d 00:00")
+        from_date = time.strftime("%Y-%m-%d 00:00:00")
     if not check_date(to_date):
-        to_date = time.strftime("%Y-%m-%d %H:%M")  # Validate date before sending it to the DB
+        to_date = time.strftime("%Y-%m-%d %H:%M:%S")  # Validate date before sending it to the DB
 
     # connect to the db
     conn = sqlite3.connect('/var/www/rpi_app/rpi_app.db')
@@ -46,11 +50,11 @@ def lab_datas_db():
     curs.execute("SELECT * FROM humidities WHERE timestamp BETWEEN ? AND ?", (from_date, to_date))
     humidities = curs.fetchall()
     conn.close()
-    return render_template("lab_datas_db.html", temp=temperatures, hum=humidities)
+    return [temperatures, humidities, from_date, to_date]
 
 def check_date(d):
     try:
-        datetime.datetime.strptime(d, '%Y-%m-%d %H:%M')
+        datetime.datetime.strptime(d, '%Y-%m-%d %H:%M:%S')
         return True
     except ValueError:
         return False
@@ -58,4 +62,3 @@ def check_date(d):
 # to run the app on port 8080
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
-
