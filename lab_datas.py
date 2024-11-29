ef6197a0a746f7ca6ad5d02890602fa041f2dd3a
Pastofarian <asegliochristophe@gmail.com>
2024-11-29 11:31:11 +0100

    fixed plotly offline

--- lab_datas.py
+++ lab_datas.py
@@ -1,20 +1,26 @@
 import sqlite3
-import Adafruit_DHT
+import smbus2
+import bme280
 
-# Function to log temperature and humidity data into the database
+# configuration for BME280 sensor
+port = 1
+address = 0x76
+bus = smbus2.SMBus(port)
+calibration_params = bme280.load_calibration_params(bus, address)
+
+# insert data into the database
 def log_datas(sensor_id, temperature, humidity):
     try:
-        # Use 'with' to ensure the connection is properly closed after use
         with sqlite3.connect('/var/www/rpi_app/rpi_app.db') as conn:
             curs = conn.cursor()
-            # Insert temperature data
+            # insert temperature datas
             curs.execute("""
-                INSERT INTO temperatures (timestamp, sensor_id, temperature) 
+                INSERT INTO temperatures (timestamp, sensor_id, temperature)
                 VALUES (datetime(CURRENT_TIMESTAMP, 'localtime'), ?, ?)
             """, (sensor_id, temperature))
-            # Insert humidity data
+            # insert humidity datas
             curs.execute("""
-                INSERT INTO humidities (timestamp, sensor_id, humidities) 
+                INSERT INTO humidities (timestamp, sensor_id, humidities)
                 VALUES (datetime(CURRENT_TIMESTAMP, 'localtime'), ?, ?)
             """, (sensor_id, humidity))
             conn.commit()
@@ -23,11 +29,17 @@ def log_datas(sensor_id, temperature, humidity):
     except Exception as e:
         print(f"General error: {e}")
 
-# Read from the sensor
-humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 17)
-
-# Log the data, using -999 as a placeholder for failed readings
-if humidity is not None and temperature is not None:
-    log_datas("1", temperature, humidity)
-else:
+# reading datas from the sensor
+try:
+    data = bme280.sample(bus, address, calibration_params)
+    temperature = data.temperature
+    humidity = data.humidity
+    # insert datas into the db
+    if humidity is not None and temperature is not None:
+        log_datas("1", temperature, humidity)
+    else:
+        log_datas("1", -999, -999)
+except Exception as e:
+    print(f"Error reading from sensor: {e}")
     log_datas("1", -999, -999)
+
--- rpi_app.py
+++ rpi_app.py
@@ -1,20 +1,19 @@
+
 from flask import Flask, request, render_template, abort
 import time
 import datetime
 import sys
 import sqlite3
 import arrow
-#import Adafruit_DHT
+# import Adafruit_DHT
 import smbus2
 import bme280
-import board
-import plotly.plotly as py
+#import board
+#import plotly.plotly as py
 from plotly.graph_objs import *
-from twilio.twiml.messaging_response import MessagingResponse
-#from plotly.offline import plot
-#from plotly.graph_objs import Scatter, Data, Layout, Figure, XAxis, YAxis
-import subprocess
-import os
+from plotly.offline import plot
+# from plotly.graph_objs import Scatter, Data, Layout, Figure, XAxis, YAxis
+
 
 port = 1
 address = 0x76
@@ -35,11 +34,11 @@ def main_page():
 @app.route("/lab_datas")
 def lab_datas():
     # read temperature and humidity from DHT22 sensor
-    #humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 17)
+    # humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 17)
     data = bme280.sample(bus, address, calibration_params)
     temperature = data.temperature
     humidity = data.humidity
-    #pressure = data.pressure
+    # pressure = data.pressure
 
     # if valid data is received from the sensor
     if humidity is not None and temperature is not None:
@@ -57,18 +56,18 @@ def lab_datas_db():
         print(f"Error in lab_datas_db: {e}")
         abort(500)  # return 500 error if something goes wrong
 
-    timezone = request.args.get('timezone', 'Etc/UTC')
+    timezone = request.args.get('timezone', 'Europe/Brussels')  
 
     # Adjust user timezone
     time_adjusted_temperatures = []
     time_adjusted_humidities = []
     for record in temperatures:
-        # record[0] is timestamp
-        local_timedate = arrow.get(record[0], "YYYY-MM-DD HH:mm:ss").to(timezone)
+        # record[0] is timestamp in local time
+        local_timedate = arrow.get(record[0], "YYYY-MM-DD HH:mm:ss", tzinfo=timezone)
         time_adjusted_temperatures.append([local_timedate.format('DD-MM-YYYY HH:mm:ss'), record[1], record[2]])
 
     for record in humidities:
-        local_timedate = arrow.get(record[0], "YYYY-MM-DD HH:mm:ss").to(timezone)
+        local_timedate = arrow.get(record[0], "YYYY-MM-DD HH:mm:ss", tzinfo=timezone)
         time_adjusted_humidities.append([local_timedate.format('DD-MM-YYYY HH:mm:ss'), record[1], record[2]])
 
     return render_template(
@@ -83,21 +82,21 @@ def lab_datas_db():
 
 def get_datas():
     # fetch data range from request args
-    from_date = request.args.get('from') # Get the from date value from URL
-    to_date = request.args.get('to') # Get the to date value from URL
-    timezone = request.args.get('timezone', 'Etc/UTC')
+    from_date = request.args.get('from')  # Get the from date value from URL
+    to_date = request.args.get('to')      # Get the to date value from URL
+    timezone = request.args.get('timezone', 'Europe/Brussels')  
     range_time_form = request.args.get('range_time', '')  # optional range time param
 
     # check default value
     if not from_date:
-        from_date = arrow.utcnow().shift(days=-1).format("DD-MM-YYYY HH:mm:ss")
+        from_date = arrow.now(timezone).shift(days=-1).format("DD-MM-YYYY HH:mm:ss")
 
     if not to_date:
-        to_date = arrow.utcnow().format("DD-MM-YYYY HH:mm:ss")
+        to_date = arrow.now(timezone).format("DD-MM-YYYY HH:mm:ss")
 
     print("REQUEST:")
     print(request.args)
-    print ("from: %s, to: %s, timezone: %s" % (from_date, to_date, timezone))
+    print("from: %s, to: %s, timezone: %s" % (from_date, to_date, timezone))
 
     # convert range_time safely
     try:
@@ -108,10 +107,10 @@ def get_datas():
 
     if range_time_int is not None:
         # Calculate from and to date based on range_time using arrow
-        arrow_time_to = arrow.utcnow()
+        arrow_time_to = arrow.now(timezone)
         arrow_time_from = arrow_time_to.shift(hours=-range_time_int)
-        from_date_utc = arrow_time_from.strftime("%Y-%m-%d %H:%M:%S")
-        to_date_utc = arrow_time_to.strftime("%Y-%m-%d %H:%M:%S")
+        from_date_str = arrow_time_from.format("YYYY-MM-DD HH:mm:ss")
+        to_date_str = arrow_time_to.format("YYYY-MM-DD HH:mm:ss")
     else:
         try:
             # parse and adjust user-provided dates
@@ -119,18 +118,18 @@ def get_datas():
             arrow_to = arrow.get(to_date, "DD-MM-YYYY HH:mm:ss", tzinfo=timezone)
         except Exception as e:
             print(f"Error parsing dates: {e}")
-            arrow_from = arrow.utcnow().floor('day')  # default to start of current day
-            arrow_to = arrow.utcnow()  # default to current time
+            arrow_from = arrow.now(timezone).floor('day')  # default to start of current day
+            arrow_to = arrow.now(timezone)  # default to current time
 
-        from_date_utc = arrow_from.to('Etc/UTC').format("YYYY-MM-DD HH:mm:ss")
-        to_date_utc = arrow_to.to('Etc/UTC').format("YYYY-MM-DD HH:mm:ss")
+        from_date_str = arrow_from.format("YYYY-MM-DD HH:mm:ss")
+        to_date_str = arrow_to.format("YYYY-MM-DD HH:mm:ss")
 
     # db connection
     conn = sqlite3.connect('/var/www/rpi_app/rpi_app.db')
     curs = conn.cursor()
-    curs.execute("SELECT * FROM temperatures WHERE timestamp BETWEEN ? AND ?", (from_date_utc, to_date_utc))
+    curs.execute("SELECT * FROM temperatures WHERE timestamp BETWEEN ? AND ?", (from_date_str, to_date_str))
     temperatures = curs.fetchall()
-    curs.execute("SELECT * FROM humidities WHERE timestamp BETWEEN ? AND ?", (from_date_utc, to_date_utc))
+    curs.execute("SELECT * FROM humidities WHERE timestamp BETWEEN ? AND ?", (from_date_str, to_date_str))
     humidities = curs.fetchall()
     conn.close()
 
@@ -140,51 +139,39 @@ def get_datas():
 @app.route("/to_plotly", methods=['GET'])
 def to_plotly_offline():
     try:
-        # fetch data required for Plotly
+        # getting the datas
         temperatures, humidities, timezone, from_date, to_date = get_datas()
     except Exception as e:
         print(f"Error generating plot: {e}")
-        abort(500)  # return 500 error if data fetch fails
+        abort(500)
 
-    # initialize lists for time series data
-    time_series_adjusted_temperatures = []
-    time_series_temperature_values = []
-    time_series_adjusted_humidities = []
-    time_series_humidity_values = []
+    # setting datas for Plotly
+    time_series_temperature = [arrow.get(record[0], "YYYY-MM-DD HH:mm:ss").to(timezone).format('YYYY-MM-DD HH:mm:ss') for record in temperatures]
+    temperature_values = [round(record[2], 2) for record in temperatures]
 
-    # process temperature records, adjust timestamps to user timezone
-    for record in temperatures:
-        local_timedate = arrow.get(record[0], "YYYY-MM-DD HH:mm:ss").to(timezone)
-        time_series_adjusted_temperatures.append(local_timedate.format('YYYY-MM-DD HH:mm:ss'))
-        time_series_temperature_values.append(round(record[2], 2))
+    time_series_humidity = [arrow.get(record[0], "YYYY-MM-DD HH:mm:ss").to(timezone).format('YYYY-MM-DD HH:mm:ss') for record in humidities]
+    humidity_values = [round(record[2], 2) for record in humidities]
 
-    # process humidity records, adjust timestamps to user timezone
-    for record in humidities:
-        local_timedate = arrow.get(record[0], "YYYY-MM-DD HH:mm:ss").to(timezone)
-        time_series_adjusted_humidities.append(local_timedate.format('YYYY-MM-DD HH:mm:ss'))
-        time_series_humidity_values.append(round(record[2], 2))
+    # Creating graphs
+    temp_trace = Scatter(x=time_series_temperature, y=temperature_values, name='Température')
+    hum_trace = Scatter(x=time_series_humidity, y=humidity_values, name='Humidité', yaxis='y2')
 
-    # create Scatter objects for temperature and humidity
-    temp = Scatter(x=time_series_adjusted_temperatures, y=time_series_temperature_values, name='Température')
-    hum = Scatter(x=time_series_adjusted_humidities, y=time_series_humidity_values, name='Humidité', yaxis='y2')
+    data = [temp_trace, hum_trace]
 
-    data = [temp, hum]  # combine data
-
-    # layout for the plot
     layout = Layout(
         title="Température et Humidité",
-        xaxis=dict(title='Date'),
-        yaxis=dict(title='Température (°C)'),
-        yaxis2=dict(title='Humidité (%)', overlaying='y', side='right')
+        xaxis=dict(title="Date"),
+        yaxis=dict(title="Température (°C)"),
+        yaxis2=dict(title="Humidité (%)", overlaying="y", side="right")
     )
 
-    fig = dict(data=data, layout=layout)  # prepare figure for rendering
+    fig = dict(data=data, layout=layout)
 
-    # save html locally for display
+    # saving localy the graphs
     html_file_path = '/var/www/rpi_app/static/plotly_graph.html'
     plot(fig, filename=html_file_path, auto_open=False)
 
-    # return the path to the generated html file
+    # return the path
     return f"/static/plotly_graph.html"
 
 def check_date(d):
